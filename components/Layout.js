071c67433470232f25cb271bb8133b92d0dc47a3
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-15 21:43:13 +0100

    meta

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
         <div className={styles.layout}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             </Head>
-            
             <NavContainer />
             { children }
         </div>
