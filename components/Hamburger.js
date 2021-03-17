95c29772e1f18cc796395dd39b62bbccef7a825f
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-17 01:25:21 +0100

    update

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -2,14 +2,9 @@ import styles from '../styles/Hamburger.module.css'
 
 
 export default function Hamburger() {
-    const openNav = () => {
-        const menu = document.getElementById('menu')
-        console.log(menu)
-    }
-
-
+    
     return(
-        <div id={styles.hamburger} onClick={openNav}>
+        <div id={styles.hamburger}>
             &#9776;
         </div>
     )
